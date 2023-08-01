45596ca868b8c61a1431fee67a717791e2a9d408
baoking250402 <baoking2403@gmail.com>
2023-08-01 22:01:31 +0700

    Update docker, add nginx

--- BE/main.py
+++ BE/main.py
@@ -4,6 +4,7 @@ import user_serve
 from user_serve import serving_user
 from queue import Queue
 from threading import Thread
+from waitress import serve
 
 app = Flask(__name__)  
 
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     t = Thread(target=serving_user, args=())
     t.start()
     app.register_blueprint(apis)
-    app.run(debug=False, host='0.0.0.0', port='5000')   
+    serve(app, host='0.0.0.0', port='5000')   
     q.join()
     user_serve.finished_serve = True
     t.join()
